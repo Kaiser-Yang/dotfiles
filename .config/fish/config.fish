b4ee755fbd4ee4bc56a4db9c75e2ccc22c0984a2
Kaiser-Yang <624626089@qq.com>
2024-06-26 22:10:39 +0800

    Add fish mode prompt

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -127,3 +127,24 @@ set fish_cursor_visual block
 export PATH="$PATH:/opt/nvim-linux64/bin"
 
 export $NERD_FONTS=1 > /dev/null
+
+function fish_mode_prompt
+    switch $fish_bind_mode
+        case default
+            set_color --bold red
+            echo '[N] '
+        case insert
+            set_color --bold green
+            echo '[I] '
+        case replace_one
+            set_color --bold green
+            echo '[R] '
+        case visual
+            set_color --bold brmagenta
+            echo '[V] '
+        case '*'
+            set_color --bold red
+            echo '[?] '
+    end
+    set_color normal
+end
