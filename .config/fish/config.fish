b8e1a506f67fbbf03357a494739e9c020b50169f
Kaiser-Yang <624626089@qq.com>
2024-07-06 11:18:43 +0800

    Add an abbreviation grsr for git restore

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -21,10 +21,10 @@ abbr -a gpl git pull
 abbr -a grbs git rebase -i
 abbr -a gdag git log --graph --abbrev-commit --decorate --format=format:"'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)'" --all
 abbr -a gdagol git log --all --decorate --oneline --graph
-abbr -a gcco git checkout
 abbr -a gbrc git branch
 abbr -a gcln git clone
 abbr -a grst git reset
+abbr -a grsr git restore
 
 # abbreviations for jupyter
 abbr -a jpt jupyter
