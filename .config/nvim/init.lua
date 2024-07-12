07a41bd14bdac1a933c7e79cfda1250455a3f0ab
Kaiser-Yang <624626089@qq.com>
2024-07-12 14:58:47 +0800

    minimal to reproduce

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -7,7 +7,5 @@ vim.g.mapleader = ' '
 -- first time installation may fail, so we use pcall
 -- we first load all the plugins
 pcall(require, 'lazy_nvim')
-pcall(require, 'coc_dap_init.lua')
-pcall(require, 'key_mapping')
--- place this at the last so that the autocmd can be effective
 pcall(require, 'core')
+-- place this at the last so that the autocmd can be effective
--- .config/nvim/lua/plugin_config/nvimnotify_config.lua
+++
@@ -1,7 +0,0 @@
-vim.notify = require('notify')
-vim.notify.setup {
-    render = 'wrapped-compact',
-    minimum_width = 50,
-    max_width = 100,
-    timeout = 1000,
-}
