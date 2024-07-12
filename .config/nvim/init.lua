6bdac4da907ec4629c107d052b4caaf783c48d9d
Kaiser-Yang <624626089@qq.com>
2024-07-12 19:04:42 +0800

    Minimal yanky-reproduce

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,13 +1,6 @@
-
--- This must be the first thing to do, otherwise when opeing directory with nvim, nvim will quit
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 vim.g.mapleader = ' '
 
--- first time installation may fail, so we use pcall
--- we first load all the plugins
 pcall(require, 'lazy_nvim')
-pcall(require, 'coc_dap_init.lua')
-pcall(require, 'key_mapping')
--- place this at the last so that the autocmd can be effective
 pcall(require, 'core')
