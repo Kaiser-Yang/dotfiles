8057bf58c81ee9a861103cc9c9dfba0216987247
Kaiser-Yang <624626089@qq.com>
2024-07-18 09:50:47 +0800

    Fix a bug: compile and run with wrong directory

--- .config/nvim/lua/key_mapping.lua
+++ .config/nvim/lua/key_mapping.lua
@@ -513,21 +513,22 @@ function CompileRun()
     pcall(vim.cmd, 'w')
     local filename = vim.fn.expand("%")
     local filename_noext = vim.fn.expand("%:r")
+    local directory = vim.fn.fnamemodify(vim.fn.expand('%:p'), ':h')
     local command = ""
     if vim.bo.filetype == 'c' then
         command = string.format(
-            'gcc -g -Wall %s -I include -o %s.out && echo RUNNING && time ./%s.out',
-            filename, filename_noext, filename_noext)
+            'cd %s && gcc -g -Wall %s -I include -o %s.out && echo RUNNING && time ./%s.out',
+            directory, filename, filename_noext, filename_noext)
     elseif vim.bo.filetype == 'cpp' then
         command = string.format(
-            'g++ -g -Wall -std=c++17 -I include %s -o %s.out && echo RUNNING && time ./%s.out',
-            filename, filename_noext, filename_noext)
+            'cd %s && g++ -g -Wall -std=c++17 -I include %s -o %s.out && echo RUNNING && time ./%s.out',
+            directory, filename, filename_noext, filename_noext)
     elseif vim.bo.filetype == 'java' then
         command = string.format(
             'javac %s && echo RUNNING && time java %s',
             filename, filename_noext)
     elseif vim.bo.filetype == 'sh' then
-        command = string.format('time ./%s', filename)
+        command = string.format('cd %s && time ./%s', directory, filename)
     elseif vim.bo.filetype == 'python' then
         command = string.format('time python3 %s', filename)
     elseif vim.bo.filetype == 'html' then
