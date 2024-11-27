b5e8360627ac77d713f7f4b47299bf1bc55b3e61
Kaiser-Yang <624626089@qq.com>
2024-11-27 14:53:54 +0800

    Update keymapping for nvim-tree

--- .config/nvim/lua/plugin_config/nvimtree_config.lua
+++ .config/nvim/lua/plugin_config/nvimtree_config.lua
@@ -2,39 +2,81 @@ local function nvimtree_on_attach(bufnr)
     local api = require('nvim-tree.api')
 
     local function opts(desc)
-        return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true,
-            nowait = true }
+        return {
+            desc = 'nvim-tree: ' .. desc,
+            buffer = bufnr,
+            noremap = true,
+            silent = true,
+            nowait = true
+        }
     end
-
-    api.config.mappings.default_on_attach(bufnr)
 
     function EnterOnNvimTreeNode()
         local node = api.tree.get_node_under_cursor()
-        if node ~= nil and vim.fn.isdirectory(node.absolute_path) == 1 then
-            api.tree.change_root_to_node()
+        if node == nil then
+            return
+        end
+        if vim.fn.isdirectory(node.absolute_path) == 1 then
+            api.tree.change_root_to_node(node)
         else
-            api.node.open.edit()
+            api.node.open.edit(node)
+        end
+    end
+
+    function NodeSelect()
+        local node = api.tree.get_node_under_cursor()
+        if node == nil then
+            return
+        end
+        api.node.open.edit(node)
+    end
+
+    function NodeCollapse()
+        local node = api.tree.get_node_under_cursor()
+        if node == nil then
+            return
+        end
+        if vim.fn.isdirectory(node.absolute_path) == 1 then
+            if not node.open then
+                api.node.navigate.parent(node)
+            else
+                api.node.open.edit(node)
+            end
+        else
+            api.node.navigate.parent(node)
         end
     end
 
     vim.cmd [[
     setlocal splitright
     ]]
-    vim.keymap.del({ 'n' }, '<c-v>', { buffer = bufnr })
-    vim.keymap.del({ 'n' }, '<c-x>', { buffer = bufnr })
-    vim.keymap.del({ 'n' }, '<c-t>', { buffer = bufnr })
-    vim.keymap.del({ 'n' }, 'R', { buffer = bufnr })
-    vim.keymap.del({ 'n' }, '<tab>', { buffer = bufnr })
+    vim.keymap.set({ 'n' }, 'a', api.fs.create, opts('Create File or Directory'))
+    vim.keymap.set({ 'n' }, 'c', api.fs.copy.node, opts('Copy'))
+    vim.keymap.set({ 'n' }, 'x', api.fs.cut, opts('Cut'))
+    vim.keymap.set({ 'n' }, 'p', api.fs.paste, opts('Paste'))
+    vim.keymap.set({ 'n' }, 'y', api.fs.copy.filename, opts('Copy Name'))
+    vim.keymap.set({ 'n' }, 'd', api.fs.remove, opts('Delete'))
+    vim.keymap.set({ 'n' }, 'r', api.fs.rename, opts('Rename'))
+    vim.keymap.set({ 'n' }, 'R', api.fs.rename_full, opts('Rename: Full Path'))
+
+    vim.keymap.set({ 'n' }, '[g', api.node.navigate.git.prev, opts('Prev Git'))
+    vim.keymap.set({ 'n' }, ']g', api.node.navigate.git.next, opts('Next Git'))
+    vim.keymap.set({ 'n' }, ']d', api.node.navigate.diagnostics.next, opts('Next Diagnostic'))
+    vim.keymap.set({ 'n' }, '[d', api.node.navigate.diagnostics.prev, opts('Prev Diagnostic'))
 
     vim.keymap.set({ 'n' }, '<leader>l', api.node.open.vertical, opts('Open: Vertical Split'))
     vim.keymap.set({ 'n' }, '<leader>j', api.node.open.horizontal, opts('Open: Horizontal Split'))
-    vim.keymap.set({ 'n' }, '<leader>t', '<cmd>lua require("nvim-tree.api").node.open.tab()<cr><cmd>lua ToggleTermOnTabEnter()<cr>', opts('Open: New Tab'))
-    vim.keymap.set({ 'n' }, '<c-e>', api.tree.toggle, opts('Toggle'))
+    vim.keymap.set({ 'n' }, '<2-LeftMouse>', api.node.open.edit, opts('Open'))
+
     vim.keymap.set({ 'n' }, '?', api.tree.toggle_help, opts('Help'))
-    vim.keymap.set({ 'n' }, '<bs>', api.tree.change_root_to_parent, opts('Up'))
-    vim.keymap.set({ 'n' }, '<cr>', EnterOnNvimTreeNode, opts('Enter directory or file'))
+    vim.keymap.set({ 'n' }, '<c-e>', api.tree.toggle, opts('Toggle'))
     vim.keymap.set({ 'n' }, '<f5>', api.tree.reload, opts('Refresh'))
-    vim.keymap.set({ 'n' }, 'R', api.fs.rename_full, opts('Rename: Full Path'))
+    vim.keymap.set({ 'n' }, '<bs>', api.tree.change_root_to_parent, opts('Up'))
+    vim.keymap.set({ 'n' }, '<cr>', EnterOnNvimTreeNode, opts('Enter Folder or Open File'))
+    vim.keymap.set({ 'n' }, 'h', NodeCollapse, opts('Collapse Folder'))
+    vim.keymap.set({ 'n' }, 'l', NodeSelect, opts('Toggle Folder or Open File'))
+
+    -- vim.keymap.set({ 'n' }, '<leader>t', '<cmd>lua require("nvim-tree.api").node.open.tab()<cr><cmd>lua ToggleTermOnTabEnter()<cr>', opts('Open: New Tab'))
 end
 
 require("nvim-tree").setup({
