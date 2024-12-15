bc04a94419f9cc7eb6471eeee791069fe70c1f2f
Kaiser-Yang <624626089@qq.com>
2024-12-15 22:42:39 +0800

    Delete origin ocnfiguration for rime-ls

--- installer/installer_x86_64.sh
+++ installer/installer_x86_64.sh
@@ -52,7 +52,7 @@ if ! command -v neovim; then
     curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
     sudo rm -rf /opt/nvim
     sudo tar -C /opt -xzf nvim-linux64.tar.gz
-    rm nvim-linux64.tar.gz
+    rm -f nvim-linux64.tar.gz
 fi
 
 cd ~ || exit 1
