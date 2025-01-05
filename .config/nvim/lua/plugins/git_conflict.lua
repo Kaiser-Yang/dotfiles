bca6032bdfb45b8666a79d4e3d152b905df091a0
Kaiser-Yang <624626089@qq.com>
2025-01-05 18:02:45 +0800

    Fix git conflict

--- .config/nvim/lua/plugins/git_conflict.lua
+++ .config/nvim/lua/plugins/git_conflict.lua
@@ -5,24 +5,24 @@ return {
         'nvim-treesitter/nvim-treesitter',
     },
     config = function()
-        local git_conflict = require('git-conflict')
         ---@diagnostic disable-next-line: missing-fields
-        git_conflict.setup({
+        require('git-conflict').setup({
             default_mappings = false,
             disable_diagnostics = true,
         })
         local map_set = require('utils').map_set
+        local feedkeys = require('utils').feedkeys
         local ts_repeat_move = require('nvim-treesitter.textobjects.repeatable_move')
         local next_conflict_repeat, prev_conflict_repeat = ts_repeat_move.make_repeatable_move_pair(
-            git_conflict.next_conflict,
-            git_conflict.prev_conflict)
+            function() feedkeys('<plug>(git-conflict-next-conflict)', 'n') end,
+            function() feedkeys('<plug>(git-conflict-prev-conflict)', 'n') end)
         map_set({ 'n' }, ']x', next_conflict_repeat,
             { desc = 'Next git conflict' })
         map_set({ 'n' }, '[x', prev_conflict_repeat,
             { desc = 'Previous git conflict' })
         map_set({ 'n' }, 'gcc', '<plug>(git-conflict-ours)',
             { desc = 'Git keep current' })
-        map_set({ 'n' }, 'gci', '<plug>(git-conflict-theris)',
+        map_set({ 'n' }, 'gci', '<plug>(git-conflict-theirs)',
             { desc = 'Git keep incomming' })
         map_set({ 'n' }, 'gcb', '<plug>(git-conflict-both)',
             { desc = 'Git keep both' })
