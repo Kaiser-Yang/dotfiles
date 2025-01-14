076b2f2f157ccc90639cd801147ec28fe956598f
Kaiser-Yang <624626089@qq.com>
2025-01-14 16:31:42 +0800

    Remove latex for treesitter

--- .config/nvim/lua/plugins/tree_sitter.lua
+++ .config/nvim/lua/plugins/tree_sitter.lua
@@ -20,8 +20,7 @@ return {
         ---@diagnostic disable-next-line: missing-fields
         require 'nvim-treesitter.configs'.setup {
             ensure_installed = { 'c', 'cpp', 'cmake', 'lua', 'python', 'html', 'javascript', 'css',
-                'json', 'bash', 'regex', 'markdown', 'markdown_inline', 'diff', 'vimdoc', 'java',
-                'latex' },
+                'json', 'bash', 'regex', 'markdown', 'markdown_inline', 'diff', 'vimdoc', 'java', },
             sync_install = false,
             auto_install = true,
             highlight = {
