feb5832f7a9d0607fbaa94d010f9903bdca3ed0a
Kaiser-Yang <624626089@qq.com>
2025-01-16 22:05:45 +0800

    Update img_clip, fix typos

--- .config/nvim/lua/plugins/img_clip.lua
+++ .config/nvim/lua/plugins/img_clip.lua
@@ -9,14 +9,22 @@ return {
             desc = 'Paste image from system clipboard'
         },
     },
+    ft = vim.g.markdown_support_filetype,
     opts = {
-        dir_path = function()
-            return vim.fn.expand('%:t:r') .. '.assets'
-        end,
-        filetypes = {
-            markdown = {
-                template = '![$CURSOR]($FILE_PATH){: .img-fluid}',
-            }
+        default = {
+            dir_path = function()
+                return vim.fn.expand('%:t:r') .. '.assets'
+            end,
+            filetypes = {
+                markdown = {
+                    template = '![$CURSOR]($FILE_PATH){: .img-fluid}',
+                }
+            },
+            embed_image_as_base64 = false,
+            prompt_for_file_name = false,
+            drag_and_drop = {
+                insert_mode = true,
+            },
         }
     },
 }
--- .config/nvim/lua/plugins/markdown_render.lua
+++ .config/nvim/lua/plugins/markdown_render.lua
@@ -4,11 +4,11 @@ return {
         'nvim-treesitter/nvim-treesitter',
         'nvim-tree/nvim-web-devicons',
     },
-    ft = vim.g.markdown_support_filetypes,
+    ft = vim.g.markdown_support_filetype,
     ---@module 'render-markdown'
     ---@type render.md.UserConfig
     opts = {
-        file_types = vim.g.markdown_support_filetypes,
+        file_types = vim.g.markdown_support_filetype,
         html = {
             comment = {
                 conceal = false,
