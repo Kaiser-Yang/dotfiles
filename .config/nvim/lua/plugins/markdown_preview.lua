5bd0d98116787fd94ff593bf1322850e26eb6d22
Kaiser-Yang <624626089@qq.com>
2025-01-20 16:13:42 +0800

    fix: can not open markdown preview in archlinux

--- .config/nvim/lua/plugins/markdown_preview.lua
+++ .config/nvim/lua/plugins/markdown_preview.lua
@@ -1,8 +1,21 @@
+local get_browser_available = function()
+    local browser_list = {
+        'microsoft-edge-stable',
+    }
+    for _, browser in ipairs(browser_list) do
+        if vim.fn.executable(browser) == 1 then
+            return browser
+        end
+    end
+    return ''
+end
 return {
     "iamcco/markdown-preview.nvim",
     build = "cd app && yarn install",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
     ft = vim.g.markdown_support_filetype,
     init = function()
+        vim.g.mkdp_browser = get_browser_available()
         vim.g.mkdp_auto_close = 0
         vim.g.mkdp_filetypes = vim.g.markdown_support_filetype
         local map_set = require('utils').map_set
