8906d8cee6d5216e626991d64e64c026258f8834
Kaiser-Yang <624626089@qq.com>
2025-02-06 12:33:57 +0800

    feat: disable smooth cursor in bigfiles

--- .config/nvim/lua/plugins/smooth_cursor.lua
+++ .config/nvim/lua/plugins/smooth_cursor.lua
@@ -1,7 +1,24 @@
 return {
     "sphamba/smear-cursor.nvim",
-    opts = {
-        smear_to_cmd = false,
-        filetypes_disabled = { 'toggleterm' }
-    },
+    config = function()
+        require('smear_cursor').setup({
+            smear_to_cmd = false,
+        })
+        vim.api.nvim_create_autocmd('FileType', {
+            pattern = 'bigfile*,toggleterm',
+            group = 'UserDIY',
+            callback = function()
+                require('smear_cursor').enabled = false
+                -- when leaving the buffer, enable smear_cursor
+                vim.api.nvim_create_autocmd('BufLeave', {
+                    pattern = '<buffer>',
+                    group = 'UserDIY',
+                    callback = function()
+                        require('smear_cursor').enabled = true
+                    end,
+                    once = true
+                })
+            end
+        })
+    end
 }
