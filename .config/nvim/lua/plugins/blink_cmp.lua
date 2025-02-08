c7bab86b7e95042aebbd0252367b47b259972876
Kaiser-Yang <624626089@qq.com>
2025-02-08 17:31:06 +0800

    feat: customize blink-cmp-git

--- .config/nvim/lua/plugins/blink_cmp.lua
+++ .config/nvim/lua/plugins/blink_cmp.lua
@@ -1,3 +1,63 @@
+local function github_pr_or_issue_separate_output(output, is_pr)
+    --- @type blink-cmp-git.CompletionItem[]
+    local items = {}
+    local json_res = vim.json.decode(output)
+    for i = 1, #json_res do
+        items[i] = {
+            label = '#' .. tostring(json_res[i].number) ..
+                ' ' .. tostring(json_res[i].title),
+            insert_text = '#' .. tostring(json_res[i].number),
+            kind_name = tostring(json_res[i].state) .. (is_pr and 'PR' or 'Issue'),
+            documentation =
+                '#' .. tostring(json_res[i].number) ..
+                ' ' .. tostring(json_res[i].title) .. '\n' ..
+                'State: ' .. tostring(json_res[i].state) .. '\n' ..
+                'Author: ' .. tostring(json_res[i].author.login) .. '\n' ..
+                'Created at: ' .. tostring(json_res[i].createdAt) .. '\n' ..
+                'Updated at: ' .. tostring(json_res[i].updatedAt) .. '\n' ..
+                'Closed at: ' .. tostring(json_res[i].closedAt) .. '\n' ..
+                tostring(json_res[i].body)
+        }
+    end
+    return items
+end
+
+local function github_pr_or_issue_configure_score_offset(items)
+    -- Bonus to make sure items sorted as below:
+    -- open issue
+    -- open pr
+    -- closed issue
+    -- merged pr
+    -- closed pr
+    local keys = {
+        -- place `kind_name` here
+        'OPENIssue',
+        'OPENPR',
+        'CLOSEDIssue',
+        'MERGEDPR',
+        'CLOSEDPR'
+    }
+    local bonus = 999999
+    local bonus_score = {
+    }
+    for i = 1, #keys do
+        bonus_score[keys[i]] = bonus * (#keys - i)
+    end
+    for i = 1, #items do
+        local bonus_key = items[i].kind_name
+        if bonus_score[bonus_key] then
+            items[i].score_offset = bonus_score[bonus_key]
+        end
+        -- sort by number when having the same bonus score
+        local number = items[i].label:match('#(%d+)')
+        if number then
+            if items[i].score_offset == nil then
+                items[i].score_offset = 0
+            end
+            items[i].score_offset = items[i].score_offset + tonumber(number)
+        end
+    end
+end
 return {
     {
         'saghen/blink.cmp',
@@ -164,7 +224,31 @@ return {
                         end,
                         --- @module 'blink-cmp-git'
                         --- @type blink-cmp-git.Options
-                        opts = {}
+                        opts = {
+                            kind_icons = {
+                                OPENPR = '',
+                                CLOSEDPR = '',
+                                MERGEDPR = '',
+                                OPENIssue = '',
+                                CLOSEDIssue = '',
+                            },
+                            git_centers = {
+                                github = {
+                                    pull_request = {
+                                        separate_output = function(output)
+                                            return github_pr_or_issue_separate_output(output, true)
+                                        end,
+                                        configure_score_offset = github_pr_or_issue_configure_score_offset,
+                                    },
+                                    issue = {
+                                        separate_output = function(output)
+                                            return github_pr_or_issue_separate_output(output, false)
+                                        end,
+                                        configure_score_offset = github_pr_or_issue_configure_score_offset,
+                                    },
+                                }
+                            }
+                        }
                     },
                     dictionary = {
                         module = 'blink-cmp-dictionary',
