2c1cf7f252c31ce1b2bd7951bae77214a155ec08
Kaiser-Yang <624626089@qq.com>
2025-05-24 18:49:27 +0800

    refactor: lsp_config

--- .config/nvim/lua/plugins/lsp_config.lua
+++ .config/nvim/lua/plugins/lsp_config.lua
@@ -1,46 +1,48 @@
 return {
-    'neovim/nvim-lspconfig',
-    dependencies = {
-        'williamboman/mason.nvim',
-        'williamboman/mason-lspconfig.nvim',
-        'saghen/blink.cmp',
-        'nvim-java/nvim-java',
-        'aznhe21/actions-preview.nvim',
+    {
+        'neovim/nvim-lspconfig',
+        dependencies = {
+            'williamboman/mason.nvim',
+            'williamboman/mason-lspconfig.nvim',
+            'saghen/blink.cmp',
+            'aznhe21/actions-preview.nvim',
+        },
+        config = function()
+            vim.lsp.config('*', { root_markers = vim.g.root_markers })
+            vim.lsp.enable({
+                'bashls',
+                'clangd',
+                'eslint',
+                'jdtls',
+                'jsonls',
+                'lemminx',
+                'lua_ls',
+                'neocmake',
+                'pyright',
+                'rime_ls',
+                'tailwindcss',
+                'ts_ls',
+                'vue_ls',
+                'yamlls',
+                'markdown_oxide',
+            })
+            local map_set = require('utils').map_set
+            map_set(
+                { 'v', 'n' },
+                'ga',
+                require('actions-preview').code_actions,
+                { desc = 'Code action' }
+            )
+            map_set({ 'n' }, '<leader>d', 'K', { desc = 'Hover document', remap = true })
+        end,
     },
-    config = function()
-        require('java').setup({
+    {
+        'nvim-java/nvim-java',
+        ft = { 'java' },
+        opts = {
             jdk = {
                 auto_install = false,
-            },
-            notifications = {
-                dap = false,
-            },
-        })
-        vim.lsp.config('*', { root_markers = vim.g.root_markers })
-        vim.lsp.enable({
-            'bashls',
-            'clangd',
-            'eslint',
-            'jdtls',
-            'jsonls',
-            'lemminx',
-            'lua_ls',
-            'neocmake',
-            'pyright',
-            'rime_ls',
-            'tailwindcss',
-            'ts_ls',
-            'vue_ls',
-            'yamlls',
-            'markdown_oxide',
-        })
-        local map_set = require('utils').map_set
-        map_set(
-            { 'v', 'n' },
-            'ga',
-            require('actions-preview').code_actions,
-            { desc = 'Code action' }
-        )
-        map_set({ 'n' }, '<leader>d', 'K', { desc = 'Hover document', remap = true })
-    end,
+            }
+        }
+    },
 }
