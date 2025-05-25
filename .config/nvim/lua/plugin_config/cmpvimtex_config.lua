9db42ada773a7dda5b600c9b29af28736d450785
Kaiser-Yang <624626089@qq.com>
2025-05-25 20:15:28 +0800

    chore: remove useless files

--- .config/nvim/lua/plugin_config/cmpvimtex_config.lua
+++
@@ -1,5 +0,0 @@
-require('cmp_vimtex').setup({
-    -- Eventual options can be specified here.
-    -- Check out the tutorial for further details.
-})
-map_set({ 'i' }, '<c-s>', require('cmp_vimtex.search').search_menu)
--- .config/nvim/lua/plugins/smooth_scroll.lua
+++
@@ -1,7 +0,0 @@
-return {
-    'karb94/neoscroll.nvim',
-    opts = {
-        mappings = { '<C-d>', '<C-u>', 'zz', 'zt', 'zb' },
-        respect_scrolloff = false,
-    },
-}
