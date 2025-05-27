7082de9943b8a7f738531d069e9215ed44abc3c2
Kaiser-Yang <624626089@qq.com>
2025-05-27 19:54:06 +0800

    fix: recursive mapping

--- .config/nvim/lua/plugins/which_key.lua
+++ .config/nvim/lua/plugins/which_key.lua
@@ -3,11 +3,11 @@ return {
     event = 'VeryLazy',
     config = function()
         local which_key = require('which-key')
-        local map_set = require('utils').map_set
         which_key.setup({
             delay = vim.o.timeoutlen,
             sort = { 'alphanum', 'local', 'order', 'group', 'mod' },
         })
-        map_set({ 'n' }, 's', '<cmd>WhichKey n s<cr>')
+        -- local map_set = require('utils').map_set
+        -- map_set({ 'n' }, 's', '<cmd>WhichKey n s<cr>')
     end,
 }
