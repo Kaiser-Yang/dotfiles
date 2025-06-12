373e29b655734a34200e3c12971a8cd81a1bcbd9
Kaiser-Yang <624626089@qq.com>
2025-06-12 13:50:19 +0800

    feat(lua_ls): enable vim related APIs

--- .config/nvim/lsp/lua_ls.lua
+++ .config/nvim/lsp/lua_ls.lua
@@ -3,6 +3,27 @@ return {
     handlers = {
         ['$/progress'] = function(_, _, _) end,
     },
+    on_init = function(client)
+        client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+            runtime = {
+                version = 'LuaJIT',
+                path = {
+                    'lua/?.lua',
+                    'lua/?/init.lua',
+                },
+            },
+            workspace = {
+                checkThirdParty = false,
+                library = {
+                    vim.env.VIMRUNTIME,
+                    -- Depending on the usage, you might want to add additional paths
+                    -- here.
+                    -- '${3rd}/luv/library',
+                    -- '${3rd}/busted/library',
+                },
+            },
+        })
+    end,
     settings = {
         Lua = {
             completion = {
