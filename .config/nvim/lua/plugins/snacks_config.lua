e66734c420f458a9705cdcca5bfa938f8f3a7674
Kaiser-Yang <kaiserqzyue@foxmail.com>
2025-07-04 21:26:16 +0800

    feat(Snacks): disable animation in big files

--- .config/nvim/lua/plugins/snacks_config.lua
+++ .config/nvim/lua/plugins/snacks_config.lua
@@ -35,6 +35,16 @@ return {
         bigfile = {
             enabled = true,
             size = vim.g.big_file_limit,
+            -- Enable or disable features when big file detected
+            ---@param ctx {buf: number, ft:string}
+            setup = function(ctx)
+                if vim.fn.exists(':NoMatchParen') ~= 0 then vim.cmd([[NoMatchParen]]) end
+                Snacks.util.wo(0, { foldmethod = 'manual', statuscolumn = '', conceallevel = 0 })
+                vim.b.snacks_animate = false
+                vim.schedule(function()
+                    if vim.api.nvim_buf_is_valid(ctx.buf) then vim.bo[ctx.buf].syntax = ctx.ft end
+                end)
+            end,
         },
         dashboard = {
             enabled = false,
