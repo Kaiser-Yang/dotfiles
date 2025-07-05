4c305226ff57afc204dcc2b5d05dfe979ccdc3b2
Kaiser-Yang <kaiserqzyue@foxmail.com>
2025-07-05 16:17:51 +0800

    chore(key_mappings): remove <leader>ay

--- .config/nvim/lua/key_mapping.lua
+++ .config/nvim/lua/key_mapping.lua
@@ -92,7 +92,6 @@ map_set({ 'n' }, 'yaf', function()
     local line_count = vim.api.nvim_buf_line_count(0)
     vim.notify(string.format('Yanked %d lines to + register', line_count), nil, { title = 'Yank' })
 end, { desc = 'Yank all to + reg' })
-map_set({ 'n' }, '<leader>ay', 'yaf', { desc = 'Yank all to + reg', remap = true })
 map_set({ 'n' }, '<leader>sc', function()
     if vim.o.spell then
         vim.notify('Spell check disabled', nil, { title = 'Spell Check' })
