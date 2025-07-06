622a4e1ce5efb612046f65390fd40e3bab59996c
Kaiser-Yang <kaiserqzyue@foxmail.com>
2025-07-06 21:05:18 +0800

    chore(blink-rg): limitation for 100 items

--- .config/nvim/lua/plugins/blink_cmp.lua
+++ .config/nvim/lua/plugins/blink_cmp.lua
@@ -421,12 +421,17 @@ return {
                             fallback_to_regex_highlighting = true,
                         },
                         transform_items = function(_, items)
-                            return vim.tbl_filter(function(item)
+                            items = vim.tbl_filter(function(item)
                                 -- Remove items that consist of only numbers
                                 return not (
                                     item.label:match('^%d+$') or item.label:match('^%d+%.%d+$')
                                 )
                             end, items)
+                            if #items > 100 then
+                                -- Limit the number of items to 100
+                                items = vim.list_slice(items, 1, 100)
+                            end
+                            return items
                         end,
                     },
                 },
