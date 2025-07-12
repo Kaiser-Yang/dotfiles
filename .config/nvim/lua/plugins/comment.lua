46e592a8f355a3655eb197e90b25343c20dd92bb
Kaiser-Yang <kaiserqzyue@foxmail.com>
2025-07-12 15:15:40 +0800

    feat(comment): support toggle current line comment in insert mode

--- .config/nvim/lua/plugins/comment.lua
+++ .config/nvim/lua/plugins/comment.lua
@@ -48,6 +48,32 @@ return {
             '<space>c<space>',
             { desc = 'Toggle comment for current line', remap = true }
         )
+        map_set({ 'i' }, '<m-/>', function()
+            local before_line = vim.api.nvim_get_current_line()
+            local content_after_cursor = before_line:sub(vim.fn.col('.'))
+            local content_before_cursor = before_line:sub(1, vim.fn.col('.') - 1)
+            vim.schedule(function()
+                local current_line = vim.api.nvim_get_current_line()
+                local row, col = unpack(vim.api.nvim_win_get_cursor(0))
+                local after_col, before_col, first_neq_col
+                -- check if current_line ends with content_after_cursor
+                if current_line:sub(-#content_after_cursor) == content_after_cursor then
+                    after_col = #current_line - #content_after_cursor
+                end
+                if current_line:sub(1, #content_before_cursor) == content_before_cursor then
+                    before_col = #content_before_cursor
+                end
+                for i = 1, #current_line do
+                    if current_line:sub(i, i) ~= before_line:sub(i, i) then
+                        first_neq_col = i - 1
+                        break
+                    end
+                end
+                col = after_col or before_col or first_neq_col or col
+                vim.api.nvim_win_set_cursor(0, { row, col })
+            end)
+            return '<c-o><space>c<space>'
+        end, { desc = 'Toggle comment for current line', remap = true, expr = true })
         map_set(
             { 'x' },
             '<m-/>',
