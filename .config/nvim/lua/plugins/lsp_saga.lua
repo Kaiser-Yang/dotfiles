ca3656bc06f6aea5bc4834983dab68071ec81e56
Kaiser-Yang <kaiserqzyue@foxmail.com>
2025-07-15 13:36:11 +0800

    refactor(lsp_saga): lazy all the time

--- .config/nvim/lua/plugins/lsp_saga.lua
+++ .config/nvim/lua/plugins/lsp_saga.lua
@@ -1,104 +1,83 @@
+local map_set = require('utils').map_set
+local feedkeys = require('utils').feedkeys
+local comma_semicolon = require('comma_semicolon')
+local prev_diagnostic, next_diagnostic = comma_semicolon.make(
+    '<cmd>Lspsaga diagnostic_jump_prev<cr>',
+    '<cmd>Lspsaga diagnostic_jump_next<cr>'
+)
+vim.api.nvim_create_autocmd('FileType', {
+    pattern = 'sagarename',
+    callback = function()
+        map_set({ 'i', 'n' }, '<esc>', function()
+            if vim.api.nvim_get_mode().mode == 'i' then
+                feedkeys('<esc>', 'n')
+            else
+                feedkeys('<c-c>', 'm')
+            end
+        end, { buffer = true })
+    end,
+})
 return {
     'nvimdev/lspsaga.nvim',
     dependencies = {
-        'nvim-treesitter/nvim-treesitter-textobjects',
         'nvim-tree/nvim-web-devicons',
         'catppuccin/nvim',
     },
     event = 'LspAttach',
-    config = function()
-        require('lspsaga').setup({
-            callhierarchy = {
-                keys = {
-                    edit = { '<cr>' },
-                    quit = { 'q', '<esc>' },
-                },
+    opts = {
+        callhierarchy = {
+            keys = {
+                edit = { '<cr>' },
+                quit = { 'q', '<esc>' },
             },
-            definition = {
-                keys = {
-                    quit = { 'q', '<esc>'},
-                },
+        },
+        definition = {
+            keys = {
+                quit = { 'q', '<esc>' },
             },
-            ui = {
-                kind = require('catppuccin.groups.integrations.lsp_saga').custom_kind(),
+        },
+        code_action = {
+            keys = {
+                quit = { 'q', '<esc>' },
+                exec = '<cr>',
             },
-            code_action = {
-                keys = {
-                    quit = { 'q', '<esc>'},
-                    exec = '<cr>',
-                },
+        },
+        diagnostic = {
+            keys = {
+                exec_action = '<cr>',
+                quit = { 'q', '<esc>' },
+                quit_in_show = { 'q', '<esc>' },
             },
-            diagnostic = {
-                keys = {
-                    exec_action = '<cr>',
-                    quit = { 'q', '<esc>'},
-                    quit_in_show = { 'q', '<esc>'},
-                },
+        },
+        finder = {
+            keys = {
+                quit = { 'q', '<esc>' },
             },
-            finder = {
-                keys = {
-                    quit = { 'q', '<esc>'},
-                },
+        },
+        rename = {
+            in_select = false,
+            auto_save = true,
+            keys = {
+                quit = { '<c-c>' },
             },
-            rename = {
-                in_select = false,
-                auto_save = true,
-                keys = {
-                    quit = { '<c-c>' },
-                },
-            },
-            lightbulb = {
-                enable = false,
-                sign = false,
-                virtual_text = true,
-            },
-        })
-        local map_set = require('utils').map_set
-        local feedkeys = require('utils').feedkeys
-        map_set({ 'n' }, 'gd', '<cmd>Lspsaga goto_definition<cr>', { desc = 'Go to definition' })
-        map_set(
-            { 'n' },
-            'gt',
-            '<cmd>Lspsaga goto_type_definition<cr>',
-            { desc = 'Go to type definition' }
-        )
-        map_set({ 'n' }, 'gi', '<cmd>Lspsaga finder imp<cr>', { desc = 'Go to implementations' })
-        map_set({ 'n' }, 'gr', '<cmd>Lspsaga finder ref<cr>', { desc = 'Go references' })
-        map_set(
-            { 'n' },
-            '<leader>i',
-            '<cmd>Lspsaga incoming_calls<cr>',
-            { desc = 'Incoming calls' }
-        )
-        map_set(
-            { 'n' },
-            '<leader>o',
-            '<cmd>Lspsaga outgoing_calls<cr>',
-            { desc = 'Outgoing calls' }
-        )
-        map_set({ 'n' }, '<leader>R', '<cmd>Lspsaga rename mode=n<cr>', { desc = 'Rename' })
-        local next_diagnostic_repeat, prev_diagnostic_repeat = require(
-            'nvim-treesitter.textobjects.repeatable_move'
-        ).make_repeatable_move_pair(
-            function() vim.cmd('silent Lspsaga diagnostic_jump_next') end,
-            function() vim.cmd('silent Lspsaga diagnostic_jump_prev') end
-        )
-        map_set({ 'n' }, ']d', next_diagnostic_repeat, { desc = 'Next diagnostic' })
-        map_set({ 'n' }, '[d', prev_diagnostic_repeat, { desc = 'Previous diagnostic' })
-        vim.api.nvim_create_autocmd('FileType', {
-            pattern = 'sagarename',
-            callback = function()
-                map_set({ 'i', 'n' }, '<esc>', function()
-                    if
-                        vim.api.nvim_get_mode().mode == 'i'
-                        or vim.api.nvim_get_mode().mode == 'I'
-                    then
-                        feedkeys('<esc>', 'n')
-                    else
-                        feedkeys('<c-c>', 'm')
-                    end
-                end, { buffer = true })
-            end,
-        })
+        },
+        lightbulb = {
+            enable = false,
+        },
+    },
+    keys = {
+        { 'gd', '<cmd>Lspsaga goto_definition<cr>', desc = 'Go to definition' },
+        { 'gt', '<cmd>Lspsaga goto_type_definition<cr>', desc = 'Go to type definition' },
+        { 'gi', '<cmd>Lspsaga finder imp<cr>', desc = 'Go to implementations' },
+        { 'gr', '<cmd>Lspsaga finder ref<cr>', desc = 'Go references' },
+        { '<leader>o', '<cmd>Lspsaga outgoing_calls<cr>', desc = 'Outgoing calls' },
+        { '<leader>i', '<cmd>Lspsaga incoming_calls<cr>', desc = 'Incoming calls' },
+        { '<leader>R', '<cmd>Lspsaga rename mode=n<cr>', desc = 'Rename' },
+        { ']d', next_diagnostic, desc = 'Next diagnostic', expr = true, mode = { 'n', 'x', 'o' } },
+        { '[d', prev_diagnostic, desc = 'Prev diagnostic', expr = true, mode = { 'n', 'x', 'o' } },
+    },
+    config = function(_, opts)
+        opts.ui = { kind = require('catppuccin.groups.integrations.lsp_saga').custom_kind() }
+        require('lspsaga').setup(opts)
     end,
 }
