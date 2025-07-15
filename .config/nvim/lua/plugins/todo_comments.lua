583bb581fe6c2b505cd8ab25142afd06ba87e065
Kaiser-Yang <kaiserqzyue@foxmail.com>
2025-07-15 14:14:19 +0800

    refactor(todo_comments)

--- .config/nvim/lua/plugins/todo_comments.lua
+++ .config/nvim/lua/plugins/todo_comments.lua
@@ -1,44 +1,47 @@
 local utils = require('utils')
+local comma_semicolon = require('comma_semicolon')
+local prev_todo, next_todo = comma_semicolon.make(
+    function() require('todo-comments').jump_prev() end,
+    function() require('todo-comments').jump_next() end
+)
 return {
     'folke/todo-comments.nvim',
     dependencies = {
         'nvim-lua/plenary.nvim',
         'nvim-treesitter/nvim-treesitter-textobjects',
-        'Kaiser-Yang/snacks.nvim',
     },
-    config = function()
-        local todo_comments = require('todo-comments')
-        todo_comments.setup({
-            sign_priority = 1,
-            highlight = {
-                multiline = false,
+    event = 'VeryLazy',
+    opts = {
+        sign_priority = 1,
+        highlight = {
+            multiline = false,
+        },
+        search = {
+            command = 'rg',
+            args = {
+                '--color=never',
+                '--no-heading',
+                '--with-filename',
+                '--line-number',
+                '--column',
+                utils.should_ignore_hidden_files() and nil or '--hidden',
             },
-            search = {
-                command = 'rg',
-                args = {
-                    '--color=never',
-                    '--no-heading',
-                    '--with-filename',
-                    '--line-number',
-                    '--column',
-                    '--hidden',
-                },
-                pattern = [[\b(KEYWORDS):]],
-            },
-        })
-        local map_set = require('utils').map_set
-        local ts_repeat_move = require('nvim-treesitter.textobjects.repeatable_move')
-        local next_todo, prev_todo = ts_repeat_move.make_repeatable_move_pair(
-            todo_comments.jump_next,
-            todo_comments.jump_prev
-        )
-        map_set({ 'n', 'x', 'o' }, ']t', next_todo, { desc = 'Next todo comment' })
-        map_set({ 'n', 'x', 'o' }, '[t', prev_todo, { desc = 'Previous todo comment' })
-        map_set({ 'n' }, '<leader>st', function()
-            Snacks.picker.todo_comments({
-                on_show = function() vim.cmd.stopinsert() end,
-                hidden = not utils.should_ignore_hidden_files()
-            })
-        end, { desc = 'Search todo comments' })
-    end,
+            pattern = [[\b(KEYWORDS):]],
+        },
+    },
+    keys = {
+        {
+            '<leader>st',
+            function()
+                if not Snacks then return end
+                Snacks.picker.todo_comments({
+                    on_show = function() vim.cmd.stopinsert() end,
+                    hidden = not utils.should_ignore_hidden_files(),
+                })
+            end,
+            desc = 'Search todo comments',
+        },
+        { ']t', next_todo, desc = 'Next todo comment', mode = { 'n', 'x', 'o' } },
+        { '[t', prev_todo, desc = 'Previous todo comment', mode = { 'n', 'x', 'o' } },
+    },
 }
