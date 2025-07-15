f6cbb114b07978b63843d3607defb4a3994b16fa
Kaiser-Yang <kaiserqzyue@foxmail.com>
2025-07-15 20:33:55 +0800

    refactor(search)

--- .config/nvim/lua/plugins/search.lua
+++ .config/nvim/lua/plugins/search.lua
@@ -1,23 +1,15 @@
 local flash_keys = {
-    ['f'] = '<f40>',
-    ['t'] = '<f41>',
-    ['F'] = '<f42>',
-    ['T'] = '<f43>',
+    ['f'] = '<f33>',
+    ['t'] = '<f34>',
+    ['F'] = '<f35>',
+    ['T'] = '<f36>',
 }
-local utils = require('utils')
-local map_set = utils.map_set
 return {
     'Kaiser-Yang/flash.nvim',
     event = 'VeryLazy',
     branch = 'develop',
-    depedencies = {
-        'nvim-treesitter/nvim-treesitter-textobjects',
-    },
     ---@type Flash.Config
     opts = {
-        actions = {
-            [''] = function() return false end,
-        },
         search = {
             exclude = {
                 'notify',
@@ -72,61 +64,6 @@ return {
             function() require('flash').treesitter() end,
             desc = 'Flash Treesitter',
         },
-        -- {
-        -- For most terminals, this is the same as <c-/>
-        -- '<c-_>',
-        -- mode = { 'n', 'x', 'o' },
-        -- function()
-        --     local word = vim.fn.histget('/', -1)
-        --     require('flash').jump({
-        --         search = { mode = 'search' },
-        --         pattern = word,
-        --     })
-        -- end,
-        -- desc = 'Flash Search Last Searched Word',
-        -- },
-        -- {
-        --     '*',
-        --     mode = { 'n', 'x', 'o' },
-        --     function()
-        --         local word = vim.fn.expand('<cword>')
-        --         require('flash').jump({
-        --             search = {
-        --                 mode = 'search',
-        --                 forward = true,
-        --                 wrap = false,
-        --                 multi_window = false,
-        --             },
-        --             jump = {
-        --                 history = true,
-        --                 register = true,
-        --             },
-        --             pattern = word,
-        --         })
-        --     end,
-        --     desc = 'Flash Search Current Word',
-        -- },
-        -- {
-        --     '#',
-        --     mode = { 'n', 'x', 'o' },
-        --     function()
-        --         local word = vim.fn.expand('<cword>')
-        --         require('flash').jump({
-        --             search = {
-        --                 mode = 'search',
-        --                 forward = false,
-        --                 wrap = false,
-        --                 multi_window = false,
-        --             },
-        --             jump = {
-        --                 history = true,
-        --                 register = true,
-        --             },
-        --             pattern = word,
-        --         })
-        --     end,
-        --     desc = 'Flash Search Current Word Backward',
-        -- },
         {
             '<c-s>',
             mode = { 'n', 'x' },
@@ -182,64 +119,4 @@ return {
             desc = 'Flash Search Two Characters',
         },
     },
-    config = function(_, opts)
-        require('flash').setup(opts)
-        local ts_repeat_move = require('nvim-treesitter.textobjects.repeatable_move')
-        for key, _ in pairs(flash_keys) do
-            -- TODO:
-            -- only search current line for bigfiles.
-            -- To achieve this, we must update the source code of flash.nvim
-            map_set({ 'n', 'x' }, key, function()
-                if vim.bo.filetype == 'bigfile' then
-                    return ts_repeat_move['builtin_' .. key .. '_expr']()
-                end
-                ts_repeat_move['builtin_' .. key .. '_expr']()
-                vim.schedule(function()
-                    local ok, char = pcall(vim.fn.getcharstr)
-                    if ok and #char == 1 then
-                        local byte = string.byte(char)
-                        if byte >= 32 and byte <= 126 then
-                            -- Only record printable character
-                            vim.g.last_motion = key
-                            vim.g.last_motion_char = char
-                        end
-                    end
-                    char = char or ''
-                    utils.feedkeys(key .. char, 'n')
-                end)
-            end, { expr = true })
-        end
-        map_set({ 'n', 'x', 'o' }, ';', function()
-            local last_move = ts_repeat_move.last_move
-            if
-                last_move
-                and vim.tbl_contains({ 'f', 't', 'F', 'T' }, last_move.func)
-                and vim.g.last_motion
-                and vim.g.last_motion_char
-                and vim.bo.filetype ~= 'bigfile'
-            then
-                utils.feedkeys(flash_keys[vim.g.last_motion] .. vim.g.last_motion_char, 'm')
-                return
-            end
-            ts_repeat_move.repeat_last_move()
-        end)
-        map_set({ 'n', 'x', 'o' }, ',', function()
-            local last_move = ts_repeat_move.last_move
-            if
-                last_move
-                and vim.tbl_contains({ 'f', 't', 'F', 'T' }, last_move.func)
-                and vim.g.last_motion
-                and vim.g.last_motion_char
-                and vim.bo.filetype ~= 'bigfile'
-            then
-                local reversed_key = vim.g.last_motion:gsub(
-                    '%a',
-                    function(c) return c == c:lower() and c:upper() or c:lower() end
-                )
-                utils.feedkeys(flash_keys[reversed_key] .. vim.g.last_motion_char, 'm')
-                return
-            end
-            ts_repeat_move.repeat_last_move_opposite()
-        end)
-    end,
 }
