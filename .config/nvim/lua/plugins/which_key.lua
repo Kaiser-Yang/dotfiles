283d74aa88e203f7e59bc745d9a7c1d919f7cf0c
Kaiser-Yang <kaiserqzyue@foxmail.com>
2025-07-19 23:43:35 +0800

    refactor(which-key)

--- .config/nvim/lua/plugins/which_key.lua
+++ .config/nvim/lua/plugins/which_key.lua
@@ -1,13 +1,16 @@
 return {
     'folke/which-key.nvim',
     event = 'VeryLazy',
-    config = function()
-        local which_key = require('which-key')
-        which_key.setup({
-            delay = vim.o.timeoutlen,
-            sort = { 'alphanum', 'local', 'order', 'group', 'mod' },
-        })
-        local map_set = require('utils').map_set
-        map_set({ 'n' }, 's', '<cmd>WhichKey n s<cr>')
-    end,
+    opts = {
+        delay = vim.o.timeoutlen,
+        sort = { 'alphanum', 'local', 'order', 'group', 'mod' },
+    },
+    keys = {
+        {
+            '<leader>?',
+            function() require('which-key').show() end,
+            desc = 'Buffer Local Keymaps (which-key)',
+        },
+        { 's', '<cmd>WhichKey n s<cr>' },
+    },
 }
