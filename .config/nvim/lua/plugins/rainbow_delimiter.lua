b144777749852e587211310895db23999cf022fe
Kaiser-Yang <kaiserqzyue@foxmail.com>
2025-07-20 17:35:06 +0800

    feat(rainbow): disable in big files

--- .config/nvim/lua/plugins/rainbow_delimiter.lua
+++ .config/nvim/lua/plugins/rainbow_delimiter.lua
@@ -1,5 +1,16 @@
+-- PERF:
+-- This plugin may cause performance issues with large files.
+vim.api.nvim_create_autocmd('BufEnter', {
+    group = 'UserDIY',
+    callback = function(args)
+        if require('utils').is_big_file(args.buf) then
+            require('rainbow-delimiters').disable(args.buf)
+        end
+    end,
+})
 return {
     'HiPhish/rainbow-delimiters.nvim',
+    event = 'VeryLazy',
     init = function()
         vim.g.rainbow_delimiters = {
             highlight = {
