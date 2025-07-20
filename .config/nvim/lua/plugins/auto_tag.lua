db7d446c988b6960eba505cd67f4fdf40fb39d0f
Kaiser-Yang <kaiserqzyue@foxmail.com>
2025-07-20 19:22:52 +0800

    feat(auto_tag): disable in large files

--- .config/nvim/lua/plugins/auto_tag.lua
+++ .config/nvim/lua/plugins/auto_tag.lua
@@ -23,4 +23,15 @@ return {
             enable_close_on_slash = true,
         },
     },
+    config = function(_, opts)
+        require('nvim-ts-autotag').setup(opts)
+        vim.api.nvim_create_autocmd('BufEnter', {
+            group = 'UserDIY',
+            callback = function(args)
+                if require('utils').is_big_file(args.buf) then
+                    require('nvim-ts-autotag.internal').detach(args.buf)
+                end
+            end,
+        })
+    end,
 }
