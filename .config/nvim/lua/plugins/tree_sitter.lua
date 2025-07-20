5406c54f3dc04a727264ff74298d738fecf5abc7
Kaiser-Yang <kaiserqzyue@foxmail.com>
2025-07-20 18:02:57 +0800

    feat(tree_sitter): disable text-objects in big files

--- .config/nvim/lua/plugins/tree_sitter.lua
+++ .config/nvim/lua/plugins/tree_sitter.lua
@@ -2,14 +2,14 @@
 -- enable ts automatically??
 -- Now we can use <leader>ts to toggle treesitter
 --
--- vim.api.nvim_create_autocmd('FileType', {
---     pattern = '*', -- WARN: update this to only the filetypes you want
---     callback = function()
---         vim.treesitter.start()
---         vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
---         vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
---     end,
--- })
+vim.api.nvim_create_autocmd('FileType', {
+    pattern = '*',
+    callback = function(args)
+        if require('utils').is_big_file(args.buf) then return end
+        vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+        pcall(vim.treesitter.start)
+    end,
+})
 local function text_objects_select_wrapper(query_string, query_group)
     return function()
         require('nvim-treesitter-textobjects.select').select_textobject(query_string, query_group)
@@ -90,6 +90,7 @@ local prev_if_end, next_if_end = comma_semicolon.make(
     text_objects_move_wrapper('previous', 'end', '@conditional.outer'),
     text_objects_move_wrapper('next', 'end', '@conditional.outer')
 )
+local big_file_checker_wrapper = require('utils').big_file_checker_wrapper
 return {
     'nvim-treesitter/nvim-treesitter',
     branch = 'main',
@@ -124,323 +125,323 @@ return {
     keys = {
         {
             'af',
-            text_objects_select_wrapper('@function.outer'),
+            big_file_checker_wrapper(text_objects_select_wrapper('@function.outer')),
             desc = 'Select around function',
             mode = { 'x', 'o' },
         },
         {
             'if',
-            text_objects_select_wrapper('@function.inner'),
+            big_file_checker_wrapper(text_objects_select_wrapper('@function.inner')),
             desc = 'Select inside function',
             mode = { 'x', 'o' },
         },
         {
             'ac',
-            text_objects_select_wrapper('@class.outer'),
+            big_file_checker_wrapper(text_objects_select_wrapper('@class.outer')),
             mode = { 'x', 'o' },
             desc = 'Select around class',
         },
         {
             'ic',
-            text_objects_select_wrapper('@class.inner'),
+            big_file_checker_wrapper(text_objects_select_wrapper('@class.inner')),
             mode = { 'x', 'o' },
             desc = 'Select inside class',
         },
         {
             'ab',
-            text_objects_select_wrapper('@block.outer'),
+            big_file_checker_wrapper(text_objects_select_wrapper('@block.outer')),
             mode = { 'x', 'o' },
             desc = 'Select around block',
         },
         {
             'ib',
-            text_objects_select_wrapper('@block.inner'),
+            big_file_checker_wrapper(text_objects_select_wrapper('@block.inner')),
             mode = { 'x', 'o' },
             desc = 'Select inside block',
         },
         {
             'ai',
-            text_objects_select_wrapper('@conditional.outer'),
+            big_file_checker_wrapper(text_objects_select_wrapper('@conditional.outer')),
             mode = { 'x', 'o' },
             desc = 'Select around if',
         },
         {
             'ii',
-            text_objects_select_wrapper('@conditional.inner'),
+            big_file_checker_wrapper(text_objects_select_wrapper('@conditional.inner')),
             mode = { 'x', 'o' },
             desc = 'Select inside if',
         },
         {
             'al',
-            text_objects_select_wrapper('@loop.outer'),
+            big_file_checker_wrapper(text_objects_select_wrapper('@loop.outer')),
             mode = { 'x', 'o' },
             desc = 'Select around loop',
         },
         {
             'il',
-            text_objects_select_wrapper('@loop.inner'),
+            big_file_checker_wrapper(text_objects_select_wrapper('@loop.inner')),
             mode = { 'x', 'o' },
             desc = 'Select inside loop',
         },
         {
             'ar',
-            text_objects_select_wrapper('@return.outer'),
+            big_file_checker_wrapper(text_objects_select_wrapper('@return.outer')),
             mode = { 'x', 'o' },
             desc = 'Select around return',
         },
         {
             'ir',
-            text_objects_select_wrapper('@return.inner'),
+            big_file_checker_wrapper(text_objects_select_wrapper('@return.inner')),
             mode = { 'x', 'o' },
             desc = 'Select inside return',
         },
         {
             'ap',
-            text_objects_select_wrapper('@parameter.outer'),
+            big_file_checker_wrapper(text_objects_select_wrapper('@parameter.outer')),
             mode = { 'x', 'o' },
             desc = 'Select around parameter',
         },
         {
             'ip',
-            text_objects_select_wrapper('@parameter.inner'),
+            big_file_checker_wrapper(text_objects_select_wrapper('@parameter.inner')),
             mode = { 'x', 'o' },
             desc = 'Select inside parameter',
         },
         {
             'snf',
-            text_objects_swap_wrapper('next', '@function.outer'),
+            big_file_checker_wrapper(text_objects_swap_wrapper('next', '@function.outer')),
             desc = 'Swap with next function',
         },
         {
             'snc',
-            text_objects_swap_wrapper('next', '@class.outer'),
+            big_file_checker_wrapper(text_objects_swap_wrapper('next', '@class.outer')),
             desc = 'Swap with next class',
         },
         {
             'snl',
-            text_objects_swap_wrapper('next', '@loop.outer'),
+            big_file_checker_wrapper(text_objects_swap_wrapper('next', '@loop.outer')),
             desc = 'Swap with next loop',
         },
         {
             'snb',
-            text_objects_swap_wrapper('next', '@block.outer'),
+            big_file_checker_wrapper(text_objects_swap_wrapper('next', '@block.outer')),
             desc = 'Swap with next block',
         },
         {
             'snr',
-            text_objects_swap_wrapper('next', '@return.outer'),
+            big_file_checker_wrapper(text_objects_swap_wrapper('next', '@return.outer')),
             desc = 'Swap with next return',
         },
         {
             'snp',
-            text_objects_swap_wrapper('next', '@parameter.inner'),
+            big_file_checker_wrapper(text_objects_swap_wrapper('next', '@parameter.inner')),
             desc = 'Swap with next parameter',
         },
         {
             'sni',
-            text_objects_swap_wrapper('next', '@conditional.outer'),
+            big_file_checker_wrapper(text_objects_swap_wrapper('next', '@conditional.outer')),
             desc = 'Swap with next if',
         },
         {
             'spf',
-            text_objects_swap_wrapper('previous', '@function.outer'),
+            big_file_checker_wrapper(text_objects_swap_wrapper('previous', '@function.outer')),
             desc = 'Swap with previous function',
         },
         {
             'spc',
-            text_objects_swap_wrapper('previous', '@class.outer'),
+            big_file_checker_wrapper(text_objects_swap_wrapper('previous', '@class.outer')),
             desc = 'Swap with previous class',
         },
         {
             'spl',
-            text_objects_swap_wrapper('previous', '@loop.outer'),
+            big_file_checker_wrapper(text_objects_swap_wrapper('previous', '@loop.outer')),
             desc = 'Swap with previous loop',
         },
         {
             'spb',
-            text_objects_swap_wrapper('previous', '@block.outer'),
+            big_file_checker_wrapper(text_objects_swap_wrapper('previous', '@block.outer')),
             desc = 'Swap with previous block',
         },
         {
             'spr',
-            text_objects_swap_wrapper('previous', '@return.outer'),
+            big_file_checker_wrapper(text_objects_swap_wrapper('previous', '@return.outer')),
             desc = 'Swap with previous return',
         },
         {
             'spp',
-            text_objects_swap_wrapper('previous', '@parameter.inner'),
+            big_file_checker_wrapper(text_objects_swap_wrapper('previous', '@parameter.inner')),
             desc = 'Swap with previous parameter',
         },
         {
             'spi',
-            text_objects_swap_wrapper('previous', '@conditional.outer'),
+            big_file_checker_wrapper(text_objects_swap_wrapper('previous', '@conditional.outer')),
             desc = 'Swap with previous if',
         },
         {
             '[f',
-            prev_function_start,
+            big_file_checker_wrapper(prev_function_start),
             desc = 'Previous function start',
             mode = { 'n', 'x', 'o' },
         },
         {
             ']f',
-            next_function_start,
+            big_file_checker_wrapper(next_function_start),
             desc = 'Next function start',
             mode = { 'n', 'x', 'o' },
         },
         {
             '[c',
-            prev_class_start,
+            big_file_checker_wrapper(prev_class_start),
             desc = 'Previous class start',
             mode = { 'n', 'x', 'o' },
         },
         {
             ']c',
-            next_class_start,
+            big_file_checker_wrapper(next_class_start),
             desc = 'Next class start',
             mode = { 'n', 'x', 'o' },
         },
         {
             '[l',
-            prev_loop_start,
+            big_file_checker_wrapper(prev_loop_start),
             desc = 'Previous loop start',
             mode = { 'n', 'x', 'o' },
         },
         {
             ']l',
-            next_loop_start,
+            big_file_checker_wrapper(next_loop_start),
             desc = 'Next loop start',
             mode = { 'n', 'x', 'o' },
         },
         {
             '[b',
-            prev_block_start,
+            big_file_checker_wrapper(prev_block_start),
             desc = 'Previous block start',
             mode = { 'n', 'x', 'o' },
         },
         {
             ']b',
-            next_block_start,
+            big_file_checker_wrapper(next_block_start),
             desc = 'Next block start',
             mode = { 'n', 'x', 'o' },
         },
         {
             '[r',
-            prev_return_start,
+            big_file_checker_wrapper(prev_return_start),
             desc = 'Previous return start',
             mode = { 'n', 'x', 'o' },
         },
         {
             ']r',
-            next_return_start,
+            big_file_checker_wrapper(next_return_start),
             desc = 'Next return start',
             mode = { 'n', 'x', 'o' },
         },
         {
             '[p',
-            prev_parameter_start,
+            big_file_checker_wrapper(prev_parameter_start),
             desc = 'Previous parameter start',
             mode = { 'n', 'x', 'o' },
         },
         {
             ']p',
-            next_parameter_start,
+            big_file_checker_wrapper(next_parameter_start),
             desc = 'Next parameter start',
             mode = { 'n', 'x', 'o' },
         },
         {
             '[i',
-            prev_if_start,
+            big_file_checker_wrapper(prev_if_start),
             desc = 'Previous if start',
             mode = { 'n', 'x', 'o' },
         },
         {
             ']i',
-            next_if_start,
+            big_file_checker_wrapper(next_if_start),
             desc = 'Next if start',
             mode = { 'n', 'x', 'o' },
         },
         {
             '[F',
-            prev_function_end,
+            big_file_checker_wrapper(prev_function_end),
             desc = 'Previous function end',
             mode = { 'n', 'x', 'o' },
         },
         {
             ']F',
-            next_function_end,
+            big_file_checker_wrapper(next_function_end),
             desc = 'Next function end',
             mode = { 'n', 'x', 'o' },
         },
         {
             '[C',
-            prev_class_end,
+            big_file_checker_wrapper(prev_class_end),
             desc = 'Previous class end',
             mode = { 'n', 'x', 'o' },
         },
         {
             ']C',
-            next_class_end,
+            big_file_checker_wrapper(next_class_end),
             desc = 'Next class end',
             mode = { 'n', 'x', 'o' },
         },
         {
             '[L',
-            prev_loop_end,
+            big_file_checker_wrapper(prev_loop_end),
             desc = 'Previous loop end',
             mode = { 'n', 'x', 'o' },
         },
         {
             ']L',
-            next_loop_end,
+            big_file_checker_wrapper(next_loop_end),
             desc = 'Next loop end',
             mode = { 'n', 'x', 'o' },
         },
         {
             '[B',
-            prev_block_end,
+            big_file_checker_wrapper(prev_block_end),
             desc = 'Previous block end',
             mode = { 'n', 'x', 'o' },
         },
         {
             ']B',
-            next_block_end,
+            big_file_checker_wrapper(next_block_end),
             desc = 'Next block end',
             mode = { 'n', 'x', 'o' },
         },
         {
             '[R',
-            prev_return_end,
+            big_file_checker_wrapper(prev_return_end),
             desc = 'Previous return end',
             mode = { 'n', 'x', 'o' },
         },
         {
             ']R',
-            next_return_end,
+            big_file_checker_wrapper(next_return_end),
             desc = 'Next return end',
             mode = { 'n', 'x', 'o' },
         },
         {
             '[P',
-            prev_parameter_end,
+            big_file_checker_wrapper(prev_parameter_end),
             desc = 'Previous parameter end',
             mode = { 'n', 'x', 'o' },
         },
         {
             ']P',
-            next_parameter_end,
+            big_file_checker_wrapper(next_parameter_end),
             desc = 'Next parameter end',
             mode = { 'n', 'x', 'o' },
         },
         {
             '[I',
-            prev_if_end,
+            big_file_checker_wrapper(prev_if_end),
             desc = 'Previous if end',
             mode = { 'n', 'x', 'o' },
         },
         {
             ']I',
-            next_if_end,
+            big_file_checker_wrapper(next_if_end),
             desc = 'Next if end',
             mode = { 'n', 'x', 'o' },
         },
