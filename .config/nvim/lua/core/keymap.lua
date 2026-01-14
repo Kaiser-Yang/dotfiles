b393c58c7ae25dc7b0696729dfd02400282667c1
kaiser <kaiserqzyue@foxmail.com>
2026-01-14 10:21:43 +0800

    feat(nvim): support for wrap line

--- .config/nvim/lua/core/keymap.lua
+++ .config/nvim/lua/core/keymap.lua
@@ -5,3 +5,12 @@ vim.keymap.set({ 'c' }, '<m-f>', '<s-right>', { noremap = true, silent = false }
 vim.keymap.set({ 'c' }, '<c-b>', '<left>', { noremap = true, silent = false })
 vim.keymap.set({ 'c' }, '<c-f>', '<right>', { noremap = true, silent = false })
 vim.keymap.set({ 'i' }, '<c-h>', '<bs>', { remap = true })
+for _, ch in pairs({ '0', '^', '$' }) do
+  vim.keymap.set({ 'n', 'x', 'v' }, ch, function()
+    if vim.v.virtnum ~= 0 then
+      return 'g' .. ch
+    else
+      return ch
+    end
+  end, { expr = true, silent = true, noremap = true })
+end
