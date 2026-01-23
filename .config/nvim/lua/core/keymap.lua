052979395875a1030260e196a2600fbd623cceab
kaiser <kaiserqzyue@foxmail.com>
2026-01-23 21:19:26 +0800

    chore: utils

--- .config/nvim/lua/core/keymap.lua
+++ .config/nvim/lua/core/keymap.lua
@@ -1,37 +1,38 @@
-vim.keymap.set({ 'c' }, '<m-p>', '<up>', { noremap = true, silent = false })
-vim.keymap.set({ 'c' }, '<m-n>', '<down>', { noremap = true, silent = false })
-vim.keymap.set({ 'c' }, '<m-b>', '<s-left>', { noremap = true, silent = false })
-vim.keymap.set({ 'c' }, '<m-f>', '<s-right>', { noremap = true, silent = false })
-vim.keymap.set({ 'c' }, '<c-b>', '<left>', { noremap = true, silent = false })
-vim.keymap.set({ 'c' }, '<c-f>', '<right>', { noremap = true, silent = false })
-vim.keymap.set({ 'i' }, '<c-h>', '<bs>', { remap = true })
+local util = require('util')
+util.key.set({ 'c' }, '<m-p>', '<up>', { silent = false })
+util.key.set({ 'c' }, '<m-n>', '<down>', { silent = false })
+util.key.set({ 'c' }, '<m-b>', '<s-left>', { silent = false })
+util.key.set({ 'c' }, '<m-f>', '<s-right>', { silent = false })
+util.key.set({ 'c' }, '<c-b>', '<left>', { silent = false })
+util.key.set({ 'c' }, '<c-f>', '<right>', { silent = false })
+util.key.set({ 'i' }, '<c-h>', '<bs>', { remap = true })
 for _, ch in pairs({ '0', '^', '$' }) do
-  vim.keymap.set({ 'n', 'x', 'v' }, ch, function()
+  util.key.set({ 'n', 'x', 'v' }, ch, function()
     if vim.v.virtnum ~= 0 then
       return 'g' .. ch
     else
       return ch
     end
-  end, { expr = true, silent = true, noremap = true })
+  end, { expr = true })
 end
 local function clear_hlsearch()
   local v = vim.o.hlsearch
   vim.o.hlsearch = false
   return vim.schedule_wrap(function() vim.o.hlsearch = v end)
 end
-vim.keymap.set('n', ']]', function()
+util.key.set('n', ']]', function()
   clear_hlsearch()()
   return 'j0[[%/{<CR>'
-end, { expr = true, silent = true, remap = true, desc = 'Jump to next section or {' })
-vim.keymap.set('n', '[[', function()
+end, { expr = true, remap = true, desc = 'Jump to next section or {' })
+util.key.set('n', '[[', function()
   clear_hlsearch()()
   return '?{<CR>w99[{'
-end, { expr = true, silent = true, desc = 'Jump to previous section or {' })
-vim.keymap.set('n', '][', function()
+end, { expr = true, desc = 'Jump to previous section or {' })
+util.key.set('n', '][', function()
   clear_hlsearch()()
   return '/}<CR>b99]}'
-end, { expr = true, silent = true, desc = 'Jump to next section or }' })
-vim.keymap.set('n', '[]', function()
+end, { expr = true, desc = 'Jump to next section or }' })
+util.key.set('n', '[]', function()
   clear_hlsearch()()
   return 'k$][%?}<CR>'
-end, { expr = true, silent = true, remap = true, desc = 'Jump to previous section or }' })
+end, { expr = true, remap = true, desc = 'Jump to previous section or }' })
--- .config/nvim/lua/util/key.lua
+++ .config/nvim/lua/util/key.lua
@@ -7,4 +7,15 @@ function M.feedkeys(keys, mode)
   vim.api.nvim_feedkeys(termcodes, mode, false)
 end
 
+--- Set a map
+--- @param mode string|string[]?
+--- @param lhs string
+--- @param rhs string|function
+--- @param opts? vim.keymap.set.Opts default: { silent = true, remap = false, nowait = true }
+function M.set(mode, lhs, rhs, opts)
+  opts = vim.tbl_extend('force', { silent = true, remap = false, nowait = true }, opts or {})
+  mode = mode or 'n'
+  vim.keymap.set(mode, lhs, rhs, opts)
+end
+
 return M
