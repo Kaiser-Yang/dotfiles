58c0da647617c0fb8e8ac65ef8e1fba57e3f429a
kaiser <kaiserqzyue@foxmail.com>
2026-01-24 12:11:30 +0800

    feat: add ,t and ,f for global mapping

--- .config/nvim/lua/core/keymap.lua
+++ .config/nvim/lua/core/keymap.lua
@@ -37,3 +37,21 @@ util.key.set('n', '[]', function()
   return 'k$][%?}<CR>'
 end, { expr = true, remap = true, desc = 'Jump to previous section or }' })
 util.key.set('n', '&', '<cmd>&&<cr>')
+util.key.set('i', ',t', function()
+  if util.inside_block({ 'comment' }) ~= true then
+    vim.notify('Not inside comment block')
+    return ',t'
+  end
+  return '<c-g>u``<++>' .. string.rep('<left>', 5)
+end, { expr = true })
+util.key.set('i', ',f', function()
+  local pattern = '<++>'
+  local row, _ = unpack(vim.api.nvim_win_get_cursor(0))
+  local cur_buf = vim.api.nvim_get_current_buf()
+  local row_end = math.min(row + 100, vim.api.nvim_buf_line_count(cur_buf))
+  local match = vim.fn.matchbufline(cur_buf, pattern, row, row_end)[1]
+  if match then
+    vim.api.nvim_win_set_cursor(0, { match.lnum, match.byteidx })
+    util.key.feedkeys('<c-g>u' .. string.rep('<del>', #pattern), 'n')
+  end
+end)
