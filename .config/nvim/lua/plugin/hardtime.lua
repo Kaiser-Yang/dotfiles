a1454e9653ab8de9cfc4262c4da5ff122edf4cfe
kaiser <kaiserqzyue@foxmail.com>
2026-01-28 19:47:39 +0800

    feat(hardtime): more keys for hard time

--- .config/nvim/lua/plugin/hardtime.lua
+++ .config/nvim/lua/plugin/hardtime.lua
@@ -10,5 +10,11 @@ return {
       ['<Left>'] = false,
       ['<Right>'] = false,
     },
+    hints = {
+      ['dl'] = { message = function() return 'Use x instead of dl' end, length = 2 },
+      ['dh'] = { message = function() return 'Use X instead of dh' end, length = 2 },
+      ['cl'] = { message = function() return 'Use s instead of cl' end, length = 2 },
+      ['%^C'] = { message = function() return 'Use S instead of ^C' end, length = 2 },
+    },
   },
 }
