4b762252bce64e52125306db7ea4a6dfaf2febd5
Kaiser-Yang <kaiserqzyue@foxmail.com>
2026-02-22 22:16:54 +0800

    chore(spell)

--- .config/nvim/lua/plugin/hardtime.lua
+++ .config/nvim/lua/plugin/hardtime.lua
@@ -1,15 +1,8 @@
 return {
   'm4xshen/hardtime.nvim',
-  lazy = false,
   dependencies = { 'MunifTanjim/nui.nvim' },
   opts = {
     disable_mouse = false,
-    disabled_keys = {
-      ['<Up>'] = false,
-      ['<Down>'] = false,
-      ['<Left>'] = false,
-      ['<Right>'] = false,
-    },
     hints = {
       ['dl'] = { message = function() return 'Use x instead of dl' end, length = 2 },
       ['dh'] = { message = function() return 'Use X instead of dh' end, length = 2 },
