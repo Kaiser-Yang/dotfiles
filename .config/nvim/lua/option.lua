362bc6a67a0ef4fb6a278a8274d5820993956212
Kaiser-Yang <kaiserqzyue@foxmail.com>
2026-03-01 19:20:30 +0800

    feat: treesitters

--- .config/nvim/lua/option.lua
+++ .config/nvim/lua/option.lua
@@ -6,7 +6,7 @@ vim.g.lightboat_opt = {
   --- @type string[]|string
   big_file_detection = { 'BufReadPre', 'FileType', 'BufReadPost' },
   --- @type string[]
-  treesitter_ensure_installed = {},
+  treesitter_ensure_installed = { 'c', 'cpp', 'go', 'python', 'sql', 'json', 'javascript' },
   --- @type string[]
   mason_ensure_installed = { 'lua-language-server', 'stylua' },
   override_ui_input = true,
