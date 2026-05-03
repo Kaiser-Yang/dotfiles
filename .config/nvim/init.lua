8c85632862fac0ba25a1b2d19a55cec81180c890
Kaiser-Yang <kaiserqzyue@foxmail.com>
2026-05-03 22:36:01 +0800

    chore: remove nested "lightboat_opt"

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,12 +1,10 @@
-vim.g.lightboat_opt = {
-  --- @type string[]
-  treesitter_ensure_installed = { 'c', 'cpp', 'diff', 'go', 'javascript', 'json', 'python', 'sql' },
-}
 vim.g.conform_on_save = true
 vim.g.treesitter_highlight_auto_start = true
 vim.g.treesitter_foldexpr_auto_set = true
 vim.g.big_file_limit = 3 * 1024 * 1024 -- 3 MB
 vim.g.big_file_average_every_line = nil -- Unit: B, nil for no limit
+vim.g.treesitter_ensure_installed =
+  { 'c', 'cpp', 'diff', 'go', 'javascript', 'json', 'python', 'sql' }
 
 require('vim._core.ui2').enable({ msg = { targets = 'msg' } })
 require('config')
--- .config/nvim/lua/plugin/core/treesitter.lua
+++ .config/nvim/lua/plugin/core/treesitter.lua
@@ -2,12 +2,12 @@ local u = require('utils')
 vim.pack.add({ u.gh('nvim-treesitter/nvim-treesitter', 'main') }, { confirm = false })
 
 if vim.fn.executable('tree-sitter') == 0 then return end
-if #vim.g.lightboat_opt.treesitter_ensure_installed > 0 then
+if #vim.g.treesitter_ensure_installed > 0 then
   local ts = require('nvim-treesitter')
   local installed = ts.get_installed()
   local not_installed = vim.tbl_filter(
     function(lang) return not vim.tbl_contains(installed, lang) end,
-    vim.g.lightboat_opt.treesitter_ensure_installed
+    vim.g.treesitter_ensure_installed
   )
   if #not_installed > 0 then ts.install(not_installed) end
 end
