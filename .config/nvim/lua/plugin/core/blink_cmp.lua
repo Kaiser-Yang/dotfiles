4a65291c57a2f07b265b44d37275d61ec5cb2f80
Kaiser-Yang <kaiserqzyue@foxmail.com>
2026-05-03 22:43:45 +0800

    refactor: only upper the buffer items

--- .config/nvim/lua/plugin/core/blink_cmp.lua
+++ .config/nvim/lua/plugin/core/blink_cmp.lua
@@ -33,23 +33,18 @@ require('blink.cmp').setup({
         transform_items = function(context, items)
           -- Do not convert case when searching
           if context.mode == 'cmdline' then return items end
+          local keyword = context.get_keyword()
+          if not keyword:match('^%u') then return items end
           local out = {}
           for _, item in ipairs(items) do
             --- @type string
             local raw = item.insertText
+            if raw:match('^%l+$') then
+              local text = string.upper(raw:sub(1, 1)) .. raw:sub(2)
+              item.insertText = text
+              item.label = text
+            end
             table.insert(out, item)
-            local item1 = vim.deepcopy(item)
-            item1.insertText, item1.label = raw:lower(), raw:lower()
-            table.insert(out, item1)
-            local item2 = vim.deepcopy(item)
-            item2.insertText, item2.label = raw:upper(), raw:upper()
-            table.insert(out, item2)
-            local item3 = vim.deepcopy(item)
-            item3.insertText, item3.label = raw:sub(1, 1):upper() .. raw:sub(2), raw:sub(1, 1):upper() .. raw:sub(2)
-            table.insert(out, item3)
-            local item4 = vim.deepcopy(item)
-            item4.insertText, item4.label = raw:sub(1, 1):lower() .. raw:sub(2), raw:sub(1, 1):lower() .. raw:sub(2)
-            table.insert(out, item4)
           end
           return out
         end,
