5052036114d7e9d29785b2689460faf6c489f937
Kaiser-Yang <kaiserqzyue@foxmail.com>
2026-05-05 23:21:50 +0800

    feat: latex treesitter for render markdown

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -3,8 +3,21 @@ vim.g.treesitter_highlight_auto_start = true
 vim.g.treesitter_foldexpr_auto_set = true
 vim.g.big_file_limit = 3 * 1024 * 1024 -- 3 MB
 vim.g.big_file_average_every_line = nil -- Unit: B, nil for no limit
-vim.g.treesitter_ensure_installed =
-  { 'bash', 'c', 'cpp', 'diff', 'go', 'javascript', 'json', 'python', 'sql', 'markdown_inline', 'html', 'yaml' }
+vim.g.treesitter_ensure_installed = {
+  'bash',
+  'c',
+  'cpp',
+  'diff',
+  'go',
+  'javascript',
+  'json',
+  'python',
+  'sql',
+  'markdown_inline',
+  'html',
+  'latex',
+  'yaml',
+}
 --- @type table<string, boolean>
 _G.loaded = {}
 
