7307320f363da08c01233c7482fd323186af7d25
Kaiser-Yang <kaiserqzyue@foxmail.com>
2026-05-15 22:33:37 +0800

    fix: typo max_liens --> max_lines

--- .config/nvim/lua/plugin/core/treesitter_context.lua
+++ .config/nvim/lua/plugin/core/treesitter_context.lua
@@ -2,6 +2,6 @@ local u = require('utils')
 vim.pack.add({ u.gh('nvim-treesitter/nvim-treesitter-context') }, { confirm = false })
 
 require('treesitter-context').setup({
-  max_liens = math.max(vim.o.scrolloff, 5),
+  max_lines = math.max(vim.o.scrolloff, 5),
   on_attach = function(buffer) return not u.buffer.big(buffer) end,
 })
