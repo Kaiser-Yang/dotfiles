da6cd1ae52f6dfc7f40ddf7f6c4b8be53e979bbe
Kaiser-Yang <kaiserqzyue@foxmail.com>
2026-05-17 02:41:40 +0800

    feat: DetectConflictAndLoad for loclist and open/close

--- .config/nvim/lua/config/commands.lua
+++ .config/nvim/lua/config/commands.lua
@@ -2,7 +2,33 @@ vim.schedule(function()
   local u = require('utils')
   local command = {
     DetectConflictAndLoad = {
-      callback = function()
+      callback = function(args)
+        local params = vim.split(args.args or '', '%s+', { trimempty = true })
+        local list_type = nil
+        local should_open = nil
+
+        for _, p in ipairs(params) do
+          if p == 'loclist' or p == 'qflist' then
+            if list_type ~= nil then
+              vim.notify('Only one of loclist/qflist can be specified.', vim.log.levels.ERROR, { title = 'Resolve' })
+              return
+            end
+            list_type = p
+          elseif p == 'open' or p == 'close' then
+            if should_open ~= nil then
+              vim.notify('Only one of open/close can be specified.', vim.log.levels.ERROR, { title = 'Resolve' })
+              return
+            end
+            should_open = (p == 'open')
+          else
+            vim.notify('Invalid argument: ' .. p, vim.log.levels.ERROR, { title = 'Resolve' })
+            return
+          end
+        end
+
+        if list_type == nil then list_type = 'loclist' end
+        if should_open == nil then should_open = true end
+
         local conflicts = {}
         local function extend(bufnr, new_conflicts)
           for _, conflict in ipairs(new_conflicts) do
@@ -42,20 +68,37 @@ vim.schedule(function()
                   extend(bufnr, r.detect_conflicts(bufnr, true))
                 end
               end
-              local qf_list = {}
+              if #conflicts == 0 then
+                vim.notify('No conflicts detected in current repository.', vim.log.levels.INFO, { title = 'Resolve' })
+                return
+              end
+              local items = {}
               for i, conflict in ipairs(conflicts) do
-                table.insert(qf_list, {
+                table.insert(items, {
                   bufnr = conflict.bufnr,
                   filename = vim.api.nvim_buf_get_name(conflict.bufnr),
                   lnum = conflict.start,
                   text = string.format('Conflict %d/%d', i, #conflicts),
                 })
               end
-              vim.fn.setqflist(qf_list)
+              if list_type == 'qflist' then
+                vim.fn.setqflist({}, 'r', {
+                  title = 'Resolve Conflicts',
+                  items = items,
+                })
+                if should_open then vim.cmd('copen') end
+              else
+                vim.fn.setloclist(vim.api.nvim_get_current_win(), {}, 'r', {
+                  title = 'Resolve Conflicts',
+                  items = items,
+                })
+                if should_open then vim.cmd('lopen') end
+              end
               vim.notify(
                 string.format(
-                  'Detected %d conflict(s) in current repository, they have been added to quickfix list.',
-                  #conflicts
+                  'Detected %d conflict(s) in current repository, they have been added to %s.',
+                  #conflicts,
+                  list_type
                 ),
                 vim.log.levels.INFO,
                 { title = 'Resolve' }
@@ -64,7 +107,33 @@ vim.schedule(function()
           end
         )
       end,
-      opt = { nargs = 0, bar = true },
+      opt = {
+        nargs = '*',
+        bar = true,
+        complete = function(ArgLead, CmdLine, CursorPos)
+          local all_items = { 'loclist', 'qflist', 'open', 'close' }
+          local used = {}
+          for _, token in ipairs(vim.split(CmdLine:sub(1, CursorPos), '%s+', { trimempty = true })) do
+            used[token] = true
+          end
+
+          local has_list = used.loclist or used.qflist
+          local has_open = used.open or used.close
+
+          local candidates = {}
+          for _, item in ipairs(all_items) do
+            if item:find('^' .. vim.pesc(ArgLead)) then
+              if item == 'loclist' or item == 'qflist' then
+                if not has_list and not used[item] then table.insert(candidates, item) end
+              elseif item == 'open' or item == 'close' then
+                if not has_open and not used[item] then table.insert(candidates, item) end
+              end
+            end
+          end
+
+          return candidates
+        end,
+      },
     },
     BuildPlugin = {
       callback = function(args)
