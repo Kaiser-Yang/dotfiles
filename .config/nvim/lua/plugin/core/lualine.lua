0ceb5ec92dce473faff7cc49430a3d8010aa7eaa
Kaiser-Yang <kaiserqzyue@foxmail.com>
2026-05-17 11:47:46 +0800

    feat: macro recoding infor for lua line

--- .config/nvim/lua/plugin/core/lualine.lua
+++ .config/nvim/lua/plugin/core/lualine.lua
@@ -4,13 +4,19 @@ vim.pack.add({
   u.gh('nvim-lualine/lualine.nvim'),
 }, { confirm = false })
 
+local function recording()
+  local reg = vim.fn.reg_recording()
+  if reg == '' then return '' end
+  return 'REC @' .. reg
+end
+
 require('lualine').setup({
   options = {
     globalstatus = true,
     always_divide_middle = true,
   },
   sections = {
-    lualine_c = { 'filename', 'filesize' },
+    lualine_c = { 'filename', 'filesize', recording },
     lualine_x = { 'searchcount', 'lsp_status', 'encoding', 'fileformat', 'filetype' },
   },
 })
