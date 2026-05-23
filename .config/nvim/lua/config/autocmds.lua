df11fcb977c2e6b5d232b430a0334d56139b26bb
Kaiser-Yang <kaiserqzyue@foxmail.com>
2026-05-23 16:17:20 +0800

    feat: resize nvim-tree when only one window

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -294,3 +294,18 @@ vim.schedule_wrap(vim.api.nvim_create_autocmd)({ 'BufWinEnter', 'BufEnter' }, {
     end
   end,
 })
+
+vim.schedule_wrap(vim.api.nvim_create_autocmd)('WinNew', {
+  callback = function()
+    if not _G.loaded['nvim-tree.lua'] then return end
+    local tree = require('nvim-tree.api').tree
+    if not tree.is_visible() then return end
+    local normal_win_count = 0
+    for _, win in ipairs(vim.api.nvim_tabpage_list_wins(0)) do
+      if vim.api.nvim_win_is_valid(win) and vim.api.nvim_win_get_config(win).relative == '' then
+        normal_win_count = normal_win_count + 1
+      end
+    end
+    if normal_win_count == 2 then vim.cmd('NvimTreeResize 35') end
+  end,
+})
