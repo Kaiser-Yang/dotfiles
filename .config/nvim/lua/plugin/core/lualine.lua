bcf9518056968254f5ab48fe177ce4558d6f60fa
Kaiser-Yang <kaiserqzyue@foxmail.com>
2026-05-23 16:18:48 +0800

    feat: indent infor for lualine

--- .config/nvim/lua/plugin/core/lualine.lua
+++ .config/nvim/lua/plugin/core/lualine.lua
@@ -8,13 +8,23 @@ local function recording()
   return 'REC @' .. reg
 end
 
+local function indent()
+  local expandtab = vim.bo.expandtab
+  local sw = vim.bo.shiftwidth
+  if expandtab then
+    return string.format("space:%d", sw)
+  else
+    return string.format("tab:%d", sw)
+  end
+end
+
 require('lualine').setup({
   options = {
     globalstatus = true,
     always_divide_middle = true,
   },
   sections = {
-    lualine_c = { 'filename', 'filesize', recording },
+    lualine_c = { 'filename', 'filesize', indent, recording },
     lualine_x = { 'searchcount', 'lsp_status', 'encoding', 'fileformat', 'filetype' },
   },
 })
