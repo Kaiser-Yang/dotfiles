f89c297714df80759608b11d9ccfa6837e7733ab
Kaiser-Yang <kaiserqzyue@foxmail.com>
2026-05-24 21:20:56 +0800

    chore

--- .config/nvim/lua/plugin/optional/endwise.lua
+++ .config/nvim/lua/plugin/optional/endwise.lua
@@ -1,10 +1,11 @@
 local u = require('utils')
 u.gh('RRethy/nvim-treesitter-endwise')
+local e = require('nvim-treesitter-endwise')
 
-require('nvim-treesitter-endwise').init()
+e.init()
 for _, buf in ipairs(vim.api.nvim_list_bufs()) do
   if u.buffer.normal(buf) then
     local lang = vim.treesitter.language.get_lang(vim.bo[buf].filetype)
-    if require('nvim-treesitter-endwise').is_supported(lang) then require('nvim-treesitter.endwise').attach(buf) end
+    if e.is_supported(lang) then require('nvim-treesitter.endwise').attach(buf) end
   end
 end
