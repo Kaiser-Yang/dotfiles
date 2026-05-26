e5da5aaafb17977517915020d1f075436715d926
Kaiser-Yang <kaiserqzyue@foxmail.com>
2026-05-27 03:57:31 +0800

    chore: wezterm

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,39 +1,23 @@
 local wezterm = require('wezterm')
 local act = wezterm.action
+
 -- check if a command exists
 local function command_exists(cmd)
-    local handle = io.popen('command -v ' .. cmd .. " >/dev/null 2>&1 && echo 'yes' || echo 'no'")
+    local handle = io.popen('command -v ' .. cmd .. " >&/dev/null && echo 'yes' || echo 'no'")
     local result = handle:read('*a')
     handle:close()
     return result:find('yes') ~= nil
 end
 
--- Check if WSL is available
-local function is_wsl_available()
-    local handle = io.popen('wsl.exe -l')
-    local result = handle:read('*a')
-    handle:close()
-    return result ~= nil and result ~= ''
-end
-
 -- Get the default program based on the operating system
 local function default_program()
     local default_prog = nil
-    local target = wezterm.target_triple
-    if target:find('windows') then
-        if is_wsl_available() then
-            default_prog = { 'wsl.exe' }
-        else
-            default_prog = { 'powershell.exe' }
-        end
+    if command_exists('zsh') then
+        default_prog = { 'zsh' }
+    elseif command_exists('fish') then
+        default_prog = { 'fish' }
     else
-        if command_exists('zsh') then
-            default_prog = { 'zsh' }
-        elseif command_exists('fish') then
-            default_prog = { 'fish' }
-        else
-            default_prog = { 'bash' }
-        end
+        default_prog = { 'bash' }
     end
     return default_prog
 end
@@ -53,15 +37,7 @@ return {
         },
     }),
     font_size = 14,
-    keys = {
-        { key = 'z', mods = 'CTRL|ALT', action = 'Nop' },
-        { key = '=', mods = 'CTRL|SUPER', action = act.IncreaseFontSize },
-        { key = '-', mods = 'CTRL|SUPER', action = act.DecreaseFontSize },
-        { key = '0', mods = 'CTRL|SUPER', action = act.ResetFontSize },
-        { key = 'Insert', mods = 'CTRL', action = act.CopyTo('Clipboard') },
-        { key = 'Insert', mods = 'SHIFT', action = act.PasteFrom('Clipboard') },
-        { key = 'v', mods = 'SUPER', action = act.PasteFrom('Clipboard') },
-    },
+    keys = {},
     mouse_bindings = {
         {
             event = { Up = { streak = 1, button = { WheelUp = 1 } } },
