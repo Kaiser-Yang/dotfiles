b0f4113ece9716ab00e11389342cacc4d7db691d
Kaiser-Yang <kaiserqzyue@foxmail.com>
2026-05-27 02:56:02 +0800

    chore: remove header-insertion for clangd

--- .config/nvim/after/lsp/clangd.lua
+++ .config/nvim/after/lsp/clangd.lua
@@ -47,7 +47,7 @@ end
 
 ---@type vim.lsp.Config
 return {
-  cmd = { 'clangd' },
+  cmd = { 'clangd', '-header-insertion-decorators=0' },
   filetypes = { 'c', 'cpp', 'objc', 'objcpp', 'cuda' },
   root_markers = {
     '.clangd',
