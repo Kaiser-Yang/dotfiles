04c05bda4ab9b7c8453c4593c7ebf06ecd38e8c7
Kaiser-Yang <kaiserqzyue@foxmail.com>
2026-05-28 00:14:36 +0800

    chore: win-resizer remove unused filetypes

--- .config/nvim/lua/plugin/optional/win_resizer.lua
+++ .config/nvim/lua/plugin/optional/win_resizer.lua
@@ -1,4 +1,4 @@
 local u = require('utils')
 u.gh('Kaiser-Yang/win-resizer.nvim')
 
-require('win-resizer').setup({ ignore_filetypes = { 'CompetiTest', 'neo-tree', 'NvimTree', 'Avante', 'AvanteInput' } })
+require('win-resizer').setup({ ignore_filetypes = { 'CompetiTest', 'NvimTree', 'dap-view' } })
