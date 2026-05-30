95aa9ba64d25389804455cdd5c875c916e564808
Kaiser-Yang <kaiserqzyue@foxmail.com>
2026-05-31 02:42:07 +0800

    chore: extra space for markdown

--- .config/nvim/lua/handler/markdown.lua
+++ .config/nvim/lua/handler/markdown.lua
@@ -17,12 +17,12 @@ function M.math_inline()
   if vim.fn.getcwd():find('github%.io') then char = '\\$\\$' end
   return expand(char .. ' ${1:formula} ' .. char .. '$0')
 end
-M.link = expand_wrap('[$1](${2:url})$0')
-M.bold = expand_wrap('**${1:bold}**$0')
-M.delete_line = expand_wrap('~~${1:delete}~~$0')
-M.italic = expand_wrap('*${1:italic}*$0')
+M.link = expand_wrap('[$1](${2:url}) $0')
+M.bold = expand_wrap('**${1:bold}** $0')
+M.delete_line = expand_wrap('~~${1:delete}~~ $0')
+M.italic = expand_wrap('*${1:italic}* $0')
 M.math_block = expand_wrap('\\$\\$\n${1:formula}\n\\$\\$\n\n$0')
 M.code_block = expand_wrap('```$1\n${2:code}\n```\n\n$0')
-M.code_inline = expand_wrap('`${1:code}`$0')
+M.code_inline = expand_wrap('`${1:code}` $0')
 
 return M
