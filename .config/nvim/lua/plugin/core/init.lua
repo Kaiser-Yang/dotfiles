0b04c51af3d7ce5559d7ea035088d4055f54d954
Kaiser-Yang <kaiserqzyue@foxmail.com>
2026-05-30 11:21:48 +0800

    chore: rename

--- .config/nvim/lua/plugin/core/init.lua
+++ .config/nvim/lua/plugin/core/init.lua
@@ -3,24 +3,18 @@ require('plugin.core.session')
 vim.schedule(function()
   -- Completion engine (blink)
   require('plugin.core.blink_cmp')
-  -- Indent guides (indent lines)
-  require('plugin.core.blink_indent')
   -- Highlight matching pairs (brackets/quotes, etc.)
   require('plugin.core.blink_pairs')
   -- Code formatter
   require('plugin.core.conform')
   -- Debug Adapter Protocol (DAP) integration
   require('plugin.core.dap')
-  -- Auto-insert `end` for some languages
-  require('plugin.core.endwise')
   -- Git signs in the gutter + hunk actions (per buffer)
   require('plugin.core.gitsigns')
   -- Detect/guess indentation style per buffer
   require('plugin.core.guess_indent')
   -- Lint
   require('plugin.core.lint')
-  -- Statusline
-  require('plugin.core.lualine')
   -- Global keymaps manager / layer system
   require('plugin.core.maplayer')
   -- File explorer
@@ -33,16 +27,6 @@ vim.schedule(function()
   require('plugin.core.surround')
   -- Fuzzy finder / pickers (Telescope)
   require('plugin.core.telescope')
-  -- Highlight and manage TODO/FIXME comments
-  require('plugin.core.todo_comments')
-  -- Treesitter (parser installer)
-  require('plugin.core.treesitter')
-  -- Treesitter context window
-  require('plugin.core.treesitter_context')
-  -- Treesitter textobjects (motions / swap / select)
-  require('plugin.core.treesitter_textobjects')
   -- Auto pairs plugin
   require('plugin.core.ultimate_autopair')
-  -- Show keybindings as you type (which-key)
-  require('plugin.core.which_key')
 end)
--- .config/nvim/lua/plugin/optional/init.lua
+++ .config/nvim/lua/plugin/optional/init.lua
@@ -1,19 +1,35 @@
 require('plugin.optional.colorscheme')
 vim.schedule(function()
+  -- Indent guides (indent lines)
+  require('plugin.optional.blink_indent')
   -- Competitive programming test runner
   require('plugin.optional.competitest')
   -- Database client (vim-dadbod)
   require('plugin.optional.dadbod')
   -- Winbar breadcrumbs
   require('plugin.optional.dropbar')
+  -- Auto-insert `end` for some languages
+  require('plugin.optional.endwise')
   -- LSP status UI
   require('plugin.optional.fidget')
   -- Code action indicator
   require('plugin.optional.lightbulb')
+  -- Statusline
+  require('plugin.optional.lualine')
   -- Markdown viewer
   require('plugin.optional.render_markdown')
   -- Better UI for vim.ui.input / vim.ui.select
   require('plugin.optional.nui')
+  -- Highlight and manage TODO/FIXME comments
+  require('plugin.optional.todo_comments')
+  -- Treesitter (parser installer)
+  require('plugin.optional.treesitter')
+  -- Treesitter context window
+  require('plugin.optional.treesitter_context')
+  -- Treesitter textobjects (motions / swap / select)
+  require('plugin.optional.treesitter_textobjects')
+  -- Show keybindings as you type (which-key)
+  require('plugin.optional.which_key')
   -- Easy window resizing
   require('plugin.optional.win_resizer')
 end)
