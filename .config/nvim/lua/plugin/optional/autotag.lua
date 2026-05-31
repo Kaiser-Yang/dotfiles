4840a13aad6d951399863118dab607f2b7ac1f8b
Kaiser-Yang <kaiserqzyue@foxmail.com>
2026-05-31 19:57:31 +0800

    perf: disable auto tag for large buffers

--- .config/nvim/lua/plugin/optional/autotag.lua
+++ .config/nvim/lua/plugin/optional/autotag.lua
@@ -1,3 +1,6 @@
 local u = require('utils')
 u.gh('tronikelis/ts-autotag.nvim')
-require('ts-autotag').setup({ auto_rename = { enabled = true } })
+require('ts-autotag').setup({
+  auto_rename = { enabled = true },
+  should_attach = function(buf) return not vim.b[buf].pairs end,
+})
