d6a9b6f6cd1b1cb18965467b5a39e223bd0a4789
Kaiser-Yang <kaiserqzyue@foxmail.com>
2026-06-01 14:01:13 +0800

    chore

--- .config/nvim/lua/legacy/plugins/snacks_config.lua
+++
@@ -1,6 +0,0 @@
-return {
-    keys = {
-        { '[w', prev_word_ref, mode = { 'n', 'o', 'x' }, desc = 'Previous word reference' },
-        { ']w', next_word_ref, mode = { 'n', 'o', 'x' }, desc = 'Next word reference' },
-    },
-}
