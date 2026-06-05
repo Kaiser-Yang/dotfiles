d6731f7cfa4c01f060d6f87e04d40ebfd136fea5
Kaiser-Yang <kaiserqzyue@foxmail.com>
2026-06-05 23:22:37 +0800

    chore

--- .config/nvim/lua/legacy/plugins/blink_cmp.lua
+++ .config/nvim/lua/legacy/plugins/blink_cmp.lua
@@ -475,19 +475,6 @@ return {
             return items
           end,
         },
-        ripgrep = {
-          transform_items = function(_, items)
-            items = vim.tbl_filter(function(item)
-              -- Remove items that consist of only numbers
-              return not (item.label:match('^%d+$') or item.label:match('^%d+%.%d+$'))
-            end, items)
-            if #items > 100 then
-              -- Limit the number of items to 100
-              items = vim.list_slice(items, 1, 100)
-            end
-            return items
-          end,
-        },
       },
     },
   },
