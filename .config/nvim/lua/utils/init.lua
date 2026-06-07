2cbea1eed818f7a1db75a5e48476dee59e95fa5f
Kaiser-Yang <kaiserqzyue@foxmail.com>
2026-06-07 15:19:00 +0800

    fix(utils): get error for no parser

--- .config/nvim/lua/utils/init.lua
+++ .config/nvim/lua/utils/init.lua
@@ -66,6 +66,8 @@ end
 function M.treesitter_available(bufnr, name)
   local lang = vim.treesitter.language.get_lang(vim.bo[bufnr].filetype)
   if lang == nil then return false end
+  local parser = vim.treesitter.get_parser(bufnr, lang)
+  if not parser then return false end
   if name == nil then return true end
   return vim.treesitter.query.get(lang, name) ~= nil
 end
