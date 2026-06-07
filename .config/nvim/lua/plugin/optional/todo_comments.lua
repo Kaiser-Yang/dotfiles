805d74da2ad749f0d6f73b4d20d472b932709622
Kaiser-Yang <kaiserqzyue@foxmail.com>
2026-06-07 11:29:31 +0800

    feat(todo-comments): only highlight the KEYWORDS

--- .config/nvim/lua/plugin/optional/todo_comments.lua
+++ .config/nvim/lua/plugin/optional/todo_comments.lua
@@ -2,4 +2,4 @@ local u = require('utils')
 u.gh('nvim-lua/plenary.nvim')
 u.gh('folke/todo-comments.nvim')
 
-require('todo-comments').setup({ signs = false })
+require('todo-comments').setup({ signs = false, highlight = { multiline = false, after = '' } })
